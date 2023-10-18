d71272d23fb40f838e028ce7d04fd39cad98f466
serhatblb <serhatblb23@gmail.com>
2023-10-18 22:11:51 +0300

    LEVEL 2 Challenge completed

--- 06_day/exercise.js
+++ 06_day/exercise.js
@@ -11,7 +11,8 @@ const countries = [
     'Hungary',
     'Ireland',
     'Japan',
-    'Kenya'
+    'Kenya',
+    'Turkiyeeeeeee'
     ]
     
 const webTechs = [
@@ -149,15 +150,132 @@ console.log('#'+ hexChars.join(''));
 
 // console.log(`rcb(${r},${g},${b})`)
 
-let rgb = [];
-for(const char of '123'){
-    var red = (parseInt(Math.random()*255));
-    var green = (parseInt(Math.random()*255));
-    var blue = (parseInt(Math.random()*255));
+// let rgb = [];
+// for(const char of '123'){
+//     var red = (parseInt(Math.random()*255));
+//     var green = (parseInt(Math.random()*255));
+//     var blue = (parseInt(Math.random()*255));
 
-    rgb.push(red,green,blue);
+//     rgb.push(red,green,blue);
+// }
+// console.log(`rgb(${red},${green},${blue})`);
+
+// //4
+// let capCountries = [];
+// for(const country of countries) {
+//     capCountries.push(country.toUpperCase());
+// } console.log(capCountries);
+
+//5
+// let countryNameLens = []
+// for(const country of countries){
+//     countryNameLens.push(country.length);
+// }
+// console.log(countryNameLens)
+
+//6
+let countryData = []
+for(const country of countries){
+    let len = country.length;
+    let first3Chars = country.slice(0,3).toUpperCase();
+    countryData.push([country,first3Chars,len]);
 }
-console.log(`rgb(${red},${green},${blue})`);
+for (const arr of countryData) {
+    console.log(arr);
+    }
 
-//4
-let country = ["ALBANIA", "BOLIVIA", "CANADA", "DENMARK", "ETHIOPIA", "FINLAND", "GERMANY", "HUNGARY", "IRELAND", "JAPAN", "KENYA"]
+//7
+let noCountry = []
+let landCountry = []
+
+for(const i of countries){
+    if(i.includes('land')){
+        landCountry.push(i);
+        noCountry = false;
+    }
+}
+if(noCountry){
+    console.log("There are no countries ending with land")
+}else{
+    for(const country of landCountry){
+        console.log(country);
+    }
+}
+
+//8
+let endsWith_ia = [];
+for (const country of countries){
+    if(country.endsWith('ia')){
+        endsWith_ia.push(country)
+    }   
+}
+if(endsWith_ia.length > 0){
+    for(const each of endsWith_ia)
+        console.log(each);
+}
+
+//9
+let maxLength = 0;
+let maxLengthCountry;
+
+for(const country of countries){
+    if(country.length > maxLength){
+        maxLength = country.length;
+        maxLengthCountry = country;
+    }
+}
+console.log(maxLengthCountry,maxLengthCountry.length);
+
+//10
+let lenFiveCountries = [];
+for (const country of countries) {
+    if (country.length === 5)
+        lenFiveCountries.push(country);
+}
+console.log(lenFiveCountries);
+
+//11
+let length = webTechs[0].length;
+let word;
+for(const tech of webTechs) {
+    if (tech.length > length) {
+        word = tech;    
+        length = tech.length;
+    }
+} console.log(word);
+
+//12
+let techWithLen = [];
+for (const tech of webTechs) {
+    let len = tech.length;
+    let caps = tech.toUpperCase();
+    
+    techWithLen.push([caps, len])
+};
+console.log(techWithLen);
+
+//13 
+let mernArray = ['MongoDB', 'Express', 'React', 'Node'];
+let mernStackk = [];
+
+for (const tech of mernArray) {
+    mernStackk.push(tech[0]);
+}; console.log(mernStackk.join(''));
+
+//14 
+for(const tech of webTechs) {
+    console.log(tech);
+}
+
+//15 
+let fruits = ['banana', 'orange', 'mango', 'lemon','serhat'];
+let reversedFruits = [];
+
+for (let i = fruits.length-1; i >= 0; i--) {
+    reversedFruits.push(fruits[i]);
+} console.log(reversedFruits);
+
+//16  
+for (const each of webTechs) {
+    console.log(each);
+}
