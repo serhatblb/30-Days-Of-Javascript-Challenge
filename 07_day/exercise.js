5d36470963ba3ff412351712534374507d57f8c4
serhatblb <serhatblb23@gmail.com>
2023-10-27 21:58:08 +0300

    day 7 exercise 1 completed

---
+++ 07_day/exercise.js
@@ -0,0 +1,31 @@
+// Exercise: Level 1
+
+//1
+function fullName(){
+    console.log("Serhat Bülbül")
+}
+fullName()
+
+//2
+function fullName2(firstName,lastName){
+    console.log(firstName + ' ' +lastName)
+}
+fullName2("Serhat","Bülbül")
+
+//3
+function addNumbers(a,b){
+    return a+b;
+}
+console.log(addNumbers(10,20))
+
+//4
+function areaOfRectangle(l,w){
+    return l*w
+}
+console.log(areaOfRectangle(10,20))
+
+//5
+function perimeterOfRectangle(l, w) {
+    return 2*(l+w);
+}
+console.log(perimeterOfRectangle(2, 34))
