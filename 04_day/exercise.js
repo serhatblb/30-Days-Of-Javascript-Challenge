3a398f60e351279de3875a02c2ac3c302f2f247e
serhatblb <serhatblb23@gmail.com>
2023-12-06 00:54:28 +0300

    day 4 exercises finished.

--- 04_day/exercise.js
+++ 04_day/exercise.js
@@ -14,33 +14,30 @@ let yourAge = Number(prompt("Enter your age:"));
 
 if (yourAge > myAge) {
     console.log(`You are ${yourAge - myAge} years older than me.`)
-}
-else if (myAge > yourAge) {
+} else if (myAge > yourAge) {
     console.log(`You are ${myAge - yourAge} years younger than me.`)
-}
-else {
+} else {
     console.log("We are at the same age")
 }
 
 //3
-let a = 4; let b = 3;
+let a = 4;
+let b = 3;
 //way 1
 if (a > b) {
     console.log("a is greater than b")
-}
-else if (a < b) {
+} else if (a < b) {
     console.log("a is less than b")
 }
 //way 2
-(a > b) ? console.log("a is greater than b") : console.log("a is less than b");
+(a > b) ? console.log("a is greater than b"): console.log("a is less than b");
 
 //4
 let number = Number(prompt("Enter a number: "))
 
 if (number % 2 == 0) {
     console.log(`${number} is an even number`)
-}
-else {
+} else {
     console.log(`${number} is an odd number`)
 }
 
@@ -122,3 +119,5 @@ switch (month) {
     default:
         console.log("invalid month entered");
 }
+
+// day 4 exercises finished.
