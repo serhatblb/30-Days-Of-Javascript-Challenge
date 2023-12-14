8b2e284d8585101edbcaad60453463e69855103f
serhatblb <serhatblb23@gmail.com>
2023-12-14 23:12:04 +0300

    day 22 level 1 exercise finished

---
+++ 22_day/level 1/script.js
@@ -0,0 +1,35 @@
+// Function to check if a number is prime
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 === 0 || num % 3 === 0) return false;
+
+    let i = 5;
+    while (i * i <= num) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+        i += 6;
+    }
+    return true;
+}
+
+// Get the container div
+const container = document.getElementById('numberContainer');
+
+// Generate numbers and append to the container div
+for (let i = 0; i <= 100; i++) {
+    const numberDiv = document.createElement('div');
+    numberDiv.textContent = i;
+
+    if (i % 2 === 0) {
+        numberDiv.className = 'number green-bg'; // Even numbers with green background
+
+    } else {
+        numberDiv.className = 'number yellow-bg'; // Odd numbers with yellow background
+    }
+
+    if (isPrime(i)) {
+        numberDiv.className = 'number red-bg'; // Prime numbers with red background
+    }
+
+    container.appendChild(numberDiv);
+}
